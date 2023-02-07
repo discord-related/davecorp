575444686947bed09b8be586a5f6fefd3742c94b
Danial Raza <danialrazafb@gmail.com>
2023-02-07 22:41:23 +0100

    fix(commands): correct wording in some places
    Also renamed /embeds command to /send as not all messages sent through this command are embeds.

--- src/components/commands/send.js
+++ src/components/commands/send.js
@@ -8,15 +8,15 @@ const {
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("embeds")
-        .setDescription("Sends an embed message")
+        .setName("send")
+        .setDescription("Sends the selected message from the provided options")
         .addStringOption((options) =>
             options
-                .setName("embed")
-                .setDescription("The embed to send")
+                .setName("message")
+                .setDescription("The message to send")
                 .setChoices({
-                    name: "Rules",
-                    value: "rules"
+                    name: "Rules Message",
+                    value: "rulesMessage"
                 }, {
                     name: "Order Message",
                     value: "orderMessage"
@@ -28,10 +28,10 @@ module.exports = {
         try {
             const { channel, options } = interaction;
 
-            const embed = options.getString("embed");
+            const embed = options.getString("message");
 
             switch (embed) {
-            case "rules": {
+            case "rulesMessage": {
                 channel.send({
                     content: ["Welcome to DaveCorp™️ official business server. Please read the following rules, and click the button below to accept the terms, and unlock the rest of the server.", "Same rules here as for the SurvivalMC server. Keep it nice, don't be mean/rude, and overall don't try to waste people's time."].join("\n\n"), components: [
                         new ActionRowBuilder().setComponents(
@@ -44,21 +44,21 @@ module.exports = {
                     ]
                 });
 
-                interaction.reply({ content: "Sent the rules embed.", ephemeral: true });
+                interaction.reply({ content: "Sent the rules message.", ephemeral: true });
             }
                 break;
 
             case "orderMessage": {
-                channel.send({ content: ["Hello there! This channel is for making large or complicated orders of stuff we may or may not have in stock!", "When you press the order button below you will be asked to complete a questionnaire about your order. Once that is complete, a channel will be opened for your order, and a DaveCorp™️ employee will try to respond to you quickly to discuss the price and any other questions you have.", "If you have questions about how this works, feel free to ask in #general! Thank you for your business!\n**DaveCorp™️**"].join("\n\n"), components: [
+                channel.send({ content: ["Hello there! This channel is for making large or complicated orders of stuff we may or may not have in stock!", "When you press the order button below you will be asked to complete a questionnaire about your order. Once that is complete, a channel will be opened for your order, and a DaveCorp™️ employee will try to respond to you quickly to discuss the price and any other questions you have.", "If you have questions about how this works, feel free to ask in <#1071264537979912294>! Thank you for your business!\n**DaveCorp™️**"].join("\n\n"), components: [
                     new ActionRowBuilder().setComponents(
                         new ButtonBuilder()
                             .setCustomId("orderButton")
-                            .setLabel("Order")
+                            .setLabel("Make an order")
                             .setStyle(Primary)
                     )
                 ] });
 
-                interaction.reply({ content: "Sent the order embed.", ephemeral: true });
+                interaction.reply({ content: "Sent the order message.", ephemeral: true });
             }
                 break;
             }
