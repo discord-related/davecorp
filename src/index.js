17b56b4b29b2810b1e7f39bca879b90dd583674f
Danial Raza <danialrazafb@gmail.com>
2023-02-07 22:41:49 +0100

    feat(handlers): add modal handler

--- src/index.js
+++ src/index.js
@@ -10,13 +10,16 @@ console.clear();
 const { loadButtons } = require("./handlers/buttonHandler.js");
 const { loadCommands } = require("./handlers/commandHandler.js");
 const { loadEvents } = require("./handlers/eventHandler.js");
+const { loadModals } = require("./handlers/modalHandler.js");
 
 client.buttons = new Collection();
 client.commands = new Collection();
 client.events = new Collection();
+client.modals = new Collection();
 
 loadButtons(client);
 loadEvents(client);
+loadModals(client);
 
 client.login(client.config.token).then(async () => 
     await loadCommands(client)
